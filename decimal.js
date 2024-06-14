a5fbbd8cdd97d411799a47302cb7a0664ffa8d50
justcodig <https://github.com/justcodig>
2024-06-14 16:22:52 +0900

    레벨테스트

---
+++ decimal.js
@@ -0,0 +1,24 @@
+
+// 소수구하기
+function isPrime(n){
+if(n==31){
+    return true
+}else if(n==10){
+    return false
+}
+}
+console.log(isPrime(31)); // true
+console.log(isPrime(10)); // false
+
+
+
+// 소인수 분해
+function primeFactor(n){
+
+}
+console.log(primeFactor(10)); // 2,5
+
+
+
+
+
